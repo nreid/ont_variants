553d7abdbfa79a64637ea53094f53de11d734a1d
Noah Reid <noah.reid@uconn.edu>
2021-09-01 15:11:50 -0400

    deepvariant

---
+++ scripts/long_reads/03_pepper_margin_deepvariant.sh
@@ -0,0 +1,51 @@
+#!/bin/bash 
+#SBATCH --job-name=pepper
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 12
+#SBATCH --mem=125G
+#SBATCH --qos=general
+#SBATCH --partition=general
+#SBATCH --mail-user=
+#SBATCH --mail-type=END
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+module load singularity/3.7.1
+
+# Set the number of CPUs to use
+THREADS="12"
+
+# input/output files, directories
+OUTDIR=../../results/longreads/pepper_deepvariant
+mkdir -p $OUTDIR
+
+export TMPDIR=../../results/longreads/pepper_deepvariant/tmp
+export SINGULARITY_TMPDIR=$TMPDIR
+mkdir $TMPDIR
+
+BAM=../../results/longreads/medaka_variant/alignment/coral.bam
+GENOME=../../genome/coral.fasta
+
+OUTPRE=coral_test
+VCF=coral_test.vcf.gz
+
+# Pull the docker images
+singularity pull --dir $OUTDIR docker://jmcdani20/hap.py:v0.3.12
+singularity pull --dir $OUTDIR docker://kishwars/pepper_deepvariant:r0.4
+
+
+# Run PEPPER-Margin-DeepVariant
+singularity exec --bind /usr/lib/locale/ \
+$OUTDIR/pepper_deepvariant_r0.4.sif \
+run_pepper_margin_deepvariant call_variant \
+-b $BAM \
+-f $GENOME \
+-o $OUTDIR \
+-p $OUTPRE \
+-t $THREADS \
+--ont
+
