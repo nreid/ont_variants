55fde5a8b8c905f932a545f006061cc11e4bd19d
Noah Reid <noah.reid@uconn.edu>
2021-09-10 19:57:11 -0400

    pepper

--- scripts/long_reads/04_pepper_margin_deepvariant.sh
+++ scripts/long_reads/04_pepper_margin_deepvariant.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=pepper
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -c 12
+#SBATCH -c 4
 #SBATCH --mem=125G
 #SBATCH --qos=general
 #SBATCH --partition=general
@@ -17,7 +17,7 @@ date
 module load singularity/3.7.1
 
 # Set the number of CPUs to use
-THREADS="12"
+THREADS="1"
 
 # input/output files, directories
 OUTDIR=$(pwd)/../../results/longreads/pepper_deepvariant
@@ -35,8 +35,7 @@ VCF=coral_test.vcf.gz
 
 # Pull the docker images
 singularity pull --dir $OUTDIR docker://jmcdani20/hap.py:v0.3.12
-singularity pull --dir $OUTDIR docker://kishwars/pepper_deepvariant:r0.4
-
+singularity pull --dir $OUTDIR docker://kishwars/pepper_deepvariant:r0.5
 
 # Run PEPPER-Margin-DeepVariant
 singularity exec --bind /usr/lib/locale/ \
