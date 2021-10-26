c578813451bb2044c8a720d5b010eeed383a9f96
Noah Reid <noah.reid@uconn.edu>
2021-10-26 19:43:21 -0400

    smc

--- scripts/smc/01_msmc2.sh
+++ scripts/smc/01_msmc2.sh
@@ -33,7 +33,7 @@ FAI=../../genome/coral.fasta.fai
 for SCAF in $(zcat $TARGETS | awk '$3 > 300000' | cut -f 1 | uniq)
 do
 bedtools coverage \
--a <(bcftools view -H -r $SCAF -i 'FILTER="PASS" & GT=="0/1" & TYPE=="SNP"' $VCF | awk '{ OFS="\t" } BEGIN { x=0 }{ if( $2 < x ) { x=0 } }{ b=$4$5 }{ print $1,x,$2,b }{ x=$2 }') \
+-a <(bcftools view -H -m2 -M2 -r $SCAF -i 'FILTER="PASS" & GT=="0/1" & TYPE=="SNP"' $VCF | awk '{ OFS="\t" } BEGIN { x=0 }{ if( $2 < x ) { x=0 } }{ b=$4$5 }{ print $1,x,$2,b }{ x=$2 }') \
 -b <(zcat $TARGETS) | \
 awk '{OFS="\t"}{print $1,$3,$6,$4}' \
 >$INFILEDIR/${SCAF}.txt
